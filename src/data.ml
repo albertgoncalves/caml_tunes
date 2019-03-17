3db0f568cbb215bf745b1d41c7a8ea8dadf2ed8d
albertgoncalves <goncalves.albert@gmail.com>
2019-03-17 14:37:09 -0400

    moving things around

--- src/data.ml
+++ src/data.ml
@@ -1,5 +1,81 @@
+module U = Utils
+module P = Printf
+
 type chord =
     { first : int
     ; third : int
     ; fifth : int
     }
+
+let construct (first : int) : (string -> chord option) = function
+    | "diminished" ->
+        Some
+            { first = first
+            ; third = first + 3
+            ; fifth = first + 6
+            }
+    | "minor" ->
+        Some
+            { first = first
+            ; third = first + 3
+            ; fifth = first + 7
+            }
+    | "major" ->
+        Some
+            { first = first
+            ; third = first + 4
+            ; fifth = first + 7
+            }
+    | "augmented" ->
+        Some
+            { first = first
+            ; third = first + 4
+            ; fifth = first + 8
+            }
+    | _ -> None
+
+let center (chord : chord) : chord =
+    { first = U.mod12 chord.first
+    ; third = U.mod12 chord.third
+    ; fifth = U.mod12 chord.fifth
+    }
+
+let note_to_string : (int -> string option) = function
+    | 0 -> Some "C"
+    | 1 -> Some "C#/Db"
+    | 2 -> Some "D"
+    | 3 -> Some "D#/Eb"
+    | 4 -> Some "E"
+    | 5 -> Some "F"
+    | 6 -> Some "F#/Gb"
+    | 7 -> Some "G"
+    | 8 -> Some "G#/Ab"
+    | 9 -> Some "A"
+    | 10 -> Some "A#/Bb"
+    | 11 -> Some "B"
+    | _ -> None
+
+let tonality (chord : chord) : string option =
+    let third = chord.third - chord.first |> U.mod12 in
+    let fifth = chord.fifth - chord.first |> U.mod12 in
+    match (third, fifth) with
+        | (3, 6) -> Some "diminished"
+        | (3, 7) -> Some "minor"
+        | (4, 7) -> Some "major"
+        | (4, 8) -> Some "augmented"
+        | _ -> None
+
+let chord_to_string (chord : chord) : string =
+    let note =
+        chord.first
+        |> U.mod12
+        |> note_to_string in
+    let tonality = tonality chord in
+    match (note, tonality) with
+        | (Some note, Some tonality) ->
+            P.sprintf "%s %s" note tonality
+        | _ ->
+            P.sprintf "%d %d %d"
+                chord.first
+                chord.third
+                chord.fifth
--- src/riemann.ml
+++ src/riemann.ml
@@ -7,22 +7,6 @@ module R = Random
 module S = String
 module U = Utils
 
-let center (chord : D.chord) : D.chord =
-    { first = U.mod12 chord.first
-    ; third = U.mod12 chord.third
-    ; fifth = U.mod12 chord.fifth
-    }
-
-let tonality (chord : D.chord) : string option =
-    let third = chord.third - chord.first |> U.mod12 in
-    let fifth = chord.fifth - chord.first |> U.mod12 in
-    match (third, fifth) with
-        | (3, 6) -> Some "diminished"
-        | (3, 7) -> Some "minor"
-        | (4, 7) -> Some "major"
-        | (4, 8) -> Some "augmented"
-        | _ -> None
-
 let int_to_move : (int -> string option) = function
     | 0 -> Some "P"
     | 1 -> Some "R"
@@ -32,7 +16,7 @@ let int_to_move : (int -> string option) = function
 let move (chord : D.chord) : (string option -> D.chord) = function
     | Some "P" ->
         begin
-            match tonality chord with
+            match D.tonality chord with
                 | Some "major" ->
                     { first = chord.first
                     ; third = chord.third - 1
@@ -47,7 +31,7 @@ let move (chord : D.chord) : (string option -> D.chord) = function
         end
     | Some "R" ->
         begin
-            match tonality chord with
+            match D.tonality chord with
                 | Some "major" ->
                     { first = chord.fifth + 2
                     ; third = chord.first
@@ -62,7 +46,7 @@ let move (chord : D.chord) : (string option -> D.chord) = function
         end
     | Some "L" ->
         begin
-            match tonality chord with
+            match D.tonality chord with
                 | Some "major" ->
                     { first = chord.third
                     ; third = chord.fifth
@@ -77,84 +61,25 @@ let move (chord : D.chord) : (string option -> D.chord) = function
         end
     | _ -> chord
 
-let note_to_string : (int -> string option) = function
-    | 0 -> Some "C"
-    | 1 -> Some "C#/Db"
-    | 2 -> Some "D"
-    | 3 -> Some "D#/Eb"
-    | 4 -> Some "E"
-    | 5 -> Some "F"
-    | 6 -> Some "F#/Gb"
-    | 7 -> Some "G"
-    | 8 -> Some "G#/Ab"
-    | 9 -> Some "A"
-    | 10 -> Some "A#/Bb"
-    | 11 -> Some "B"
-    | _ -> None
-
-let chord_to_string (chord : D.chord) : string =
-    let note =
-        chord.first
-        |> U.mod12
-        |> note_to_string in
-    let tonality = tonality chord in
-    match (note, tonality) with
-        | (Some note, Some tonality) ->
-            P.sprintf "%s %s" note tonality
-        | _ ->
-            P.sprintf "%d %d %d"
-                chord.first
-                chord.third
-                chord.fifth
-
-let construct (first : int) : (string -> D.chord option) = function
-    | "diminished" ->
-        Some
-            { first = first
-            ; third = first + 3
-            ; fifth = first + 6
-            }
-    | "minor" ->
-        Some
-            { first = first
-            ; third = first + 3
-            ; fifth = first + 7
-            }
-    | "major" ->
-        Some
-            { first = first
-            ; third = first + 4
-            ; fifth = first + 7
-            }
-    | "augmented" ->
-        Some
-            { first = first
-            ; third = first + 4
-            ; fifth = first + 8
-            }
-    | _ -> None
-
-let int_to_move (chord : D.chord) (n : int) : D.chord =
-    move chord (int_to_move n)
-
 let ints_to_chords (chord : D.chord) (ns : int list) : D.chord list =
     let rec loop (chord : D.chord) (accu : D.chord list)
         : (int list -> D.chord list) = function
-        | (n::ns) -> loop (int_to_move chord n) (chord::accu) ns
+        | (n::ns) ->
+            loop (move chord (int_to_move n) |> D.center) (chord::accu) ns
         | [] -> (chord::accu) in
     loop chord [] ns
 
 let main () =
     let ns = L.init 25 (fun _ -> R.int 3) in
     let message = "unable to construct initial chord" in
-    construct 0 "major"
+    D.construct 0 "major"
     |> begin function
         | Some chord ->
             begin
-                match tonality chord with
+                match D.tonality chord with
                     | Some "major" | Some "minor" ->
                         ints_to_chords chord ns
-                        |> L.map chord_to_string
+                        |> L.map D.chord_to_string
                         |> L.rev
                         |> S.concat "\n"
                     | Some tonality ->
