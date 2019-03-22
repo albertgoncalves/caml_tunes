d0c2fde1e99bc13a13e7404f3a26e6c7b3b814d0
albertgoncalves <goncalves.albert@gmail.com>
2019-03-21 20:20:11 -0400

    this works

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs; mkShell {
     name = "Ocaml";
-    buildInputs = [ ocaml-ng.ocamlPackages_4_07.ocaml
-                    ocaml-ng.ocamlPackages_4_07.findlib
-                    ocaml-ng.ocamlPackages_4_07.ocp-indent
-                    ocaml-ng.ocamlPackages_4_07.utop
-                  ];
+    buildInputs = [
+        (with ocaml-ng.ocamlPackages_4_07; [
+            ocaml
+            findlib
+            ocp-indent
+            utop
+        ])
+    ];
     shellHook = ''
         if [ $(uname -s) = "Darwin" ]; then
             alias ls='ls --color=auto'
